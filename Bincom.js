ea232e1c7387b242fb3a4973d59e2eff64241276
IGBENEMMA <165777994+IGBENEMMA@users.noreply.github.com>
2024-10-02 18:43:09 +0100

    Add files via upload

---
+++ Bincom.js
@@ -0,0 +1,1 @@
+function button1(){document.getElementById('seeless').innerHTML= " ●	B.ENG (Electrical and Electronics Engineering) – Second class upper divisionUniversity of Michigan (Coursera) 2024"}
