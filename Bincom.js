f91c22bb9bfa2678768ce357ebe8a8e21ed18bb2
IGBENEMMA <165777994+IGBENEMMA@users.noreply.github.com>
2024-11-14 02:42:49 +0100

    Update Bincom.js

--- Bincom.js
+++ Bincom.js
@@ -1,1 +1,2 @@
 function button1(){document.getElementById('seeless').innerHTML= " ●	B.ENG (Electrical and Electronics Engineering) – Second class upper divisionUniversity of Michigan (Coursera) 2024"}
+function button2(){document.getElementById('seemore').innerHTML= 'SEE MORE'}
